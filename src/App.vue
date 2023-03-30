e7773b7fbc67fe8172301751496a44854f2378bd
Javier Garrido <javier.garrido@catena.cl>
2023-03-30 19:01:58 -0300

    creacion de rick and morty

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="body">
+ <h1>Rick And Morty</h1>
+ <FilterByStatus/>
+ <FilterByName/>
+ <ModelPersonaje/>
+ <div class="navegacion">
+  <BotonAnterior/>
+  <Botonsiguiente/>
+ </div>
+ <ListCharacters/>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListCharacters from './components/ListCharacters.vue';
+import FilterByStatus from './components/FilterByStatus.vue';
+import FilterByName from './components/FilterByName.vue';
+import ModelPersonaje from './components/ModelPersonaje.vue';
+import Botonsiguiente from './components/Botonsiguiente.vue';
+import BotonAnterior from './components/BotonAnterior.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ListCharacters,
+    FilterByStatus,
+    FilterByName,
+    Botonsiguiente,
+    BotonAnterior,
+    ModelPersonaje
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root{
+  background: rgb(48,41,105);
+background: radial-gradient(circle, #302969 0%, #790973 93%);}
+h1{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: rgb(255, 255, 255);
 }
+.navegacion{
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+}
+
 </style>
